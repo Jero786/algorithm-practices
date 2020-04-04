6b8ce60a7f28f196404e3ea155aa215703668945
Jero <jero.carrizo@gmail.com>
2020-04-04 19:45:08 -0300

    master - refactoring count island names

--- count-island/index.js
+++ count-island/index.js
@@ -4,13 +4,13 @@
  * @returns {number}
  */
 const countIsland = (islandMatrix) => {
-  const newMatrix = mapIslandsToObjects(islandMatrix);
-  const length = newMatrix.length;
+  const islandWithFlags = mapIslandToFlags(islandMatrix);
+  const length = islandWithFlags.length;
   let count = 0;
 
-  const onIslandFound = (currentIsland, island) => {
-    if (!island.isVisited) {
-      island.isVisited = true;
+  const onIslandFound = (currentIsland, islandAdjacent) => {
+    if (!islandAdjacent.isVisited) {
+      islandAdjacent.isVisited = true;
       currentIsland.isVisited = true;
       count++;
     }
@@ -19,11 +19,11 @@ const countIsland = (islandMatrix) => {
   // O(n2) quadratic time
   for (let y = 0; y < length; y++) {
     for (let x = 0; x < length; x++) {
-      if (newMatrix[y][x]) {
-        askIfExistIslandTop(newMatrix, x, y, onIslandFound);
-        askIfExistIslandLeft(newMatrix, x, y, onIslandFound);
-        askIfExistIslandBottom(newMatrix, x, y, onIslandFound);
-        askIfExistIslandRight(newMatrix, x, y, onIslandFound);
+      if (islandWithFlags[y][x]) {
+        askIfExistIslandTop(islandWithFlags, x, y, onIslandFound);
+        askIfExistIslandLeft(islandWithFlags, x, y, onIslandFound);
+        askIfExistIslandBottom(islandWithFlags, x, y, onIslandFound);
+        askIfExistIslandRight(islandWithFlags, x, y, onIslandFound);
       }
     }
   }
@@ -34,10 +34,10 @@ const countIsland = (islandMatrix) => {
 function askIfExistIslandTop(matrix, x, y, callback) {
   const rowY = matrix[y - 1];
   if (rowY) {
-    const island = rowY[x];
-    if (island) {
+    const islandAdjacent = rowY[x];
+    if (islandAdjacent) {
       const currentIsland = matrix[y][x];
-      callback(currentIsland, island);
+      callback(currentIsland, islandAdjacent);
     }
   }
 }
@@ -45,10 +45,10 @@ function askIfExistIslandTop(matrix, x, y, callback) {
 function askIfExistIslandBottom(matrix, x, y, callback) {
   const rowY = matrix[y + 1];
   if (rowY) {
-    const island = rowY[x];
-    if (island) {
+    const islandAdjacent = rowY[x];
+    if (islandAdjacent) {
       const currentIsland = matrix[y][x];
-      callback(currentIsland, island);
+      callback(currentIsland, islandAdjacent);
     }
   }
 }
@@ -56,10 +56,10 @@ function askIfExistIslandBottom(matrix, x, y, callback) {
 function askIfExistIslandLeft(matrix, x, y, callback) {
   const rowY = matrix[y];
   if (rowY) {
-    const island = rowY[x - 1];
-    if (island) {
+    const islandAdjacent = rowY[x - 1];
+    if (islandAdjacent) {
       const currentIsland = matrix[y][x];
-      callback(currentIsland, island);
+      callback(currentIsland, islandAdjacent);
     }
   }
 }
@@ -67,10 +67,10 @@ function askIfExistIslandLeft(matrix, x, y, callback) {
 function askIfExistIslandRight(matrix, x, y, callback) {
   const rowY = matrix[y];
   if (rowY) {
-    const island = rowY[x + 1];
-    if (island) {
+    const islandAdjacent = rowY[x + 1];
+    if (islandAdjacent) {
       const currentIsland = matrix[y][x];
-      callback(currentIsland, island);
+      callback(currentIsland, islandAdjacent);
     }
   }
 }
@@ -80,21 +80,20 @@ function askIfExistIslandRight(matrix, x, y, callback) {
  * @param matrix
  * @returns {[[]]}
  */
-function mapIslandsToObjects(matrix) {
-  let newMatrix = [[]];
+function mapIslandToFlags(matrix) {
+  let islandWithFlags = [[]];
   for (let y = 0; y < matrix.length; y++) {
     for (let x = 0; x < matrix.length; x++) {
       if (matrix[y] && matrix[y][x]) {
-        if (!newMatrix[y]) {
-          newMatrix[y] = [];
+        if (!islandWithFlags[y]) {
+          islandWithFlags[y] = [];
         }
-        newMatrix[y][x] = {isVisited: false};
+        islandWithFlags[y][x] = {isVisited: false};
       }
     }
   }
-  return newMatrix;
+  return islandWithFlags;
 }
-
 
 module.exports = {
   countIsland,
