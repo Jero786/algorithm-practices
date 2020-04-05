28f19281fc879e6a4f80758950935206069148cb
Jero <jero.carrizo@gmail.com>
2020-04-05 08:11:19 -0300

    master - adding some comments

--- bus-music/index.test.js
+++ bus-music/index.test.js
@@ -1,11 +1,14 @@
 const {getBusMusic} = require('.');
 
 test('should calculate properly the two longer track (with additional 30 sec) before the bus tour will finish', () => {
+  // Arrange
   const duration = 10;
   const tracks = [1,3,4,6];
   
+  // Act
   const [trackOne, trackTwo] = getBusMusic(tracks, duration);
   
+  // Assert
   expect(trackOne).toEqual(3);
   expect(trackTwo).toEqual(6);
 });
