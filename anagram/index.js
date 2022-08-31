25fae1f63229549c258e2f41d5a2093e21e34259
Jeronimo Carrizo <jero.carrizo@gmail.com>
2022-08-31 06:10:37 -0300

    improved anagram

--- anagram/index.js
+++ anagram/index.js
@@ -2,47 +2,40 @@
  *
  * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
  * https://leetcode.com/problems/valid-anagram/
+ * TC: O(N)
+ * EC: O(N)
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
-function isAnagram(s, t) {
-  if (!s || !t || s.length !== t.length) return false;
-  
-  const dictionary = createDictionary(t);
-  
-  return isAnagramFromDictionary(dictionary, s);
-}
+function isAnagram(wordOne, wordTwo) {
+  if (!wordOne || !wordTwo || wordOne.length !== wordTwo.length) return false;
 
-function createDictionary(word) {
-  const dictionary = {};
-  
-  for (let i = 0; i < word.length; i++) {
-    const letter = word[i];
+  let dictionary = {};
+
+  for (const letter of wordOne) {
     if (letter in dictionary) {
       dictionary[letter]++;
     } else {
       dictionary[letter] = 1;
     }
   }
-  
-  return dictionary;
-}
 
-function isAnagramFromDictionary(dictionary, text) {
-  
-  for (let i = 0; i < text.length; i++) {
-    const letter = text[i];
-    if (letter in dictionary && dictionary[letter] > 0) {
-      dictionary[letter]--;
+  for (const letter of wordTwo) {
+    if (letter in dictionary) {
+      if (dictionary[letter] > 0) {
+        dictionary[letter]--;
+      } else {
+        return false;
+      }
     } else {
       return false;
     }
   }
-  
-  return true;
+
+  return Object.values(dictionary).every((value) => value === 0);
 }
 
 module.exports = {
   isAnagram
-}
+};
--- anagram/index.test.js
+++ anagram/index.test.js
@@ -1,11 +1,21 @@
-const {isAnagram} = require('.');
+const { isAnagram } = require('.');
 
 test('should be indicate a anagram', () => {
   // Arrange
-  const s = "anagram";
-  const t = "nagaram"
+  const s = 'anagram';
+  const t = 'nagaram';
   // Act
   const result = isAnagram(s, t);
   // Assert
   expect(result).toBeTruthy();
+});
+
+test('should be indicate a anagram case two', () => {
+  // Arrange
+  const s = 'anagram';
+  const t = 'nagarrm';
+  // Act
+  const result = isAnagram(s, t);
+  // Assert
+  expect(result).toBeFalsy();
 });
