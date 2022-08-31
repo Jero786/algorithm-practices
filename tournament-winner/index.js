99170c7de68bb05e9786aec50c1599d621cc79a0
Jeronimo Carrizo <jero.carrizo@gmail.com>
2022-08-31 07:19:58 -0300

    improved tournament winner

--- tournament-winner/index.js
+++ tournament-winner/index.js
@@ -1,35 +1,54 @@
-function createTournament(games, results) {
-  if (!games || games.length < 2) throw new Error('A Tournamen must have at least two games');
-  if (games.length !== results.length) throw new Error('The amount of games and result should be match');
-  
-  const playScoreMap = {};
-  
-  for (let gameIndex = 0; gameIndex < games.length; gameIndex++) {
-    const game = games[gameIndex];
-    const [homeTeam, awayTeam] = game;
-    const result = results[gameIndex];
-    const playerWin = result === 1 ? homeTeam : awayTeam;
-    
-    if (!playScoreMap[playerWin]) playScoreMap[playerWin] = 0;
-    
-    playScoreMap[playerWin]++
+/**
+ * There's an algorithms tournament taking place in which teams of programmers compete
+ * against each other to solve algorithmic problems as fast as possible. Teams compete
+ * in a round robin, where each team faces off against all other teams. Only two teams
+ * compete against each other at a time, and for each competition, one team is designated
+ * the home team, while the other team is the away team. In each competition there's always
+ * one winner and one loser; there are no ties. A team receives 3 points if it wins and 0 points if it loses.
+ * The winner of the tournament is the team that receives the most amount of points.
+ *
+ * TC: O(N)
+ * EC: O(N)
+ * @param {*} teams
+ * @param {*} results
+ * @returns
+ */
+function createTournament(teams, results) {
+  const resultsByTeam = {}; // {html: 0, C#: 0, Pyhton: 0}
+
+  for (const [teamOne, teamTwo] of teams) {
+    resultsByTeam[teamOne] = 0;
+    resultsByTeam[teamTwo] = 0;
   }
-  
-  return {
-    getWinner: function () {
-      const playerWon = Object.entries(playScoreMap).sort((playerOne, playerTwo) => {
-        return playerOne[1] === playerTwo[1]
-          ? 0
-          :  playerOne[1] > playerTwo[1]
-          ? -1
-          : 1
-      })[0];
-      return playerWon[0];
+
+  for (let i = 0; i < results.length; i++) {
+    const result = results[i];
+    const isAwayTeamWinner = result === 0;
+
+    const [homeTeam, awayTeam] = teams[i];
+
+    if (isAwayTeamWinner) {
+      resultsByTeam[awayTeam]++;
+    } else {
+      resultsByTeam[homeTeam]++;
     }
   }
-  
+
+  const tableResult = Object.entries(resultsByTeam);
+
+  let maxScore = Number.MIN_VALUE;
+  let teamWinner = '';
+
+  for (const [teamName, score] of tableResult) {
+    if (score > maxScore) {
+      maxScore = score;
+      teamWinner = teamName;
+    }
+  }
+
+  return teamWinner;
 }
 
 module.exports = {
   createTournament
-}
+};
--- tournament-winner/index.test.js
+++ tournament-winner/index.test.js
@@ -1,15 +1,14 @@
-const {createTournament} = require('.');
+const { createTournament } = require('.');
 
 test('Tournament', () => {
   const competitions = [
-    ["HTML", "C#"],
-    ["C#", "Python"],
-    ["Python", "HTML"],
-  ]
+    ['HTML', 'C#'],
+    ['C#', 'Python'],
+    ['Python', 'HTML']
+  ];
   const results = [0, 0, 1];
-  
+
   const tournament = createTournament(competitions, results);
-  const result = tournament.getWinner();
-  
-  expect(result).toEqual('Python');
+
+  expect(tournament).toEqual('Python');
 });
