404e4169380bb15fc2dfdd16fc80cf0cb0b3581f
Jeronimo Carrizo <jero.carrizo@gmail.com>
2022-08-31 06:47:43 -0300

    improved subsequences

--- subsequence/index.js
+++ subsequence/index.js
@@ -1,19 +1,20 @@
-
 /**
  * Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
  */
-function isValidSubsequence(array, sequence) {
-  let arrayIndex = 0;
-  let sequenceIndex = 0;
-  
-  while(arrayIndex < array.length && sequenceIndex < sequence.length) {
-    if (array[arrayIndex] === sequence[sequenceIndex]) sequenceIndex++;
-    arrayIndex++;
+function isValidSubsequence(array, subsequence) {
+  let indexArray = 0;
+  let indexSubsequence = 0;
+
+  while (indexArray < array.length && indexSubsequence < subsequence.length) {
+    if (array[indexArray] === subsequence[indexSubsequence]) {
+      indexSubsequence++;
+    }
+    indexArray++;
   }
-  
-  return sequence.length === sequenceIndex;
+
+  return subsequence.length === indexSubsequence;
 }
 
 module.exports = {
-  isValidSubsequence,
-}
+  isValidSubsequence
+};
--- subsequence/index.test.js
+++ subsequence/index.test.js
@@ -1,13 +1,25 @@
-const {isValidSubsequence} = require('.');
+const { isValidSubsequence } = require('.');
 
 test('isValidSubsequence', () => {
   // Arrange
   const array = [5, 1, 22, 25, 6, -1, 8, 10];
   const sequence = [1, 6, -1, 10];
-  
+
   // Act
   const result = isValidSubsequence(array, sequence);
-  
+
   // Assert
   expect(result).toBeTruthy();
+});
+
+test('isValidSubsequence', () => {
+  // Arrange
+  const array = [5, 1, 22, 25, 6, -1, 8, 10];
+  const sequence = [1, 6, -1, 10, 10];
+
+  // Act
+  const result = isValidSubsequence(array, sequence);
+
+  // Assert
+  expect(result).toBeFalsy();
 });
