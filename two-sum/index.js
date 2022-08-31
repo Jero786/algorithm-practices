cb069197f42578588585a0872b296bfdfb998426
Jeronimo Carrizo <jero.carrizo@gmail.com>
2022-08-31 05:29:07 -0300

    improved two sum

--- two-sum/index.js
+++ two-sum/index.js
@@ -20,7 +20,8 @@ function getTwoSumBruteForSolution(numbers, target) {
   for (let index = 0; index < numbers.length - 1; index++) {
     for (let i = 1; i < numbers.length; i++) {
       if (index === i) continue; // we don't want to compare the same number
-      if (numbers[i] + numbers[index] === target) return [numbers[i], numbers[index]].sort(sortAsc());
+      if (numbers[i] + numbers[index] === target)
+        return [numbers[i], numbers[index]].sort(sortAsc());
     }
   }
   return [];
@@ -33,27 +34,22 @@ function getTwoSumBruteForSolution(numbers, target) {
  * @returns {this}
  */
 function getTwoSum(array, targetSum) {
-  const numbersMap = array.reduce((map, number, index) => {
-    map[number] = index;
-    return map;
-  }, {})
-  
-  for (let index = 0; index < array.length; index++) {
-    const currentNumber = array[index];
-    const numberCachedTarget = targetSum - currentNumber;
-    
-    const indexComparator = numbersMap[numberCachedTarget];
-    if (indexComparator === undefined || indexComparator === index) continue;
-    
-    const currentTarget = currentNumber + array[indexComparator];
-    if (currentTarget === targetSum) {
-      return [currentNumber, numberCachedTarget].sort(sortAsc());
+  const numbersSeen = {};
+
+  for (const number of array) {
+    const potentialMatch = targetSum - number;
+
+    if (potentialMatch in numbersSeen) {
+      return [potentialMatch, number];
+    } else {
+      numbersSeen[number] = true;
     }
   }
+
   return [];
 }
 
 module.exports = {
   getTwoSum,
   getTwoSumBruteForSolution
-}
+};
